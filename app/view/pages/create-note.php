252437f4a1fe8319628e6856d37d3e6e122232c7
rollice2 <rollice2@gmail.com>
2021-10-14 10:35:28 +0700

    V1.6

--- app/view/pages/create-note.php
+++ app/view/pages/create-note.php
@@ -4,12 +4,12 @@
     <form method="post">
         <div class="mb-3">
             <label for="title" class="form-label">Tiêu đề</label>
-            <input type="text" class="form-control" id="title" name="title">
+            <input type="text" class="form-control <?= (isset($errors['title'])) ? 'is-invalid' : '' ?>" id="title" name="title">
             <div id="emailHelp" class="form-text" style="color:red"><?= (isset($errors['title'])) ? $errors['title'] : ''; ?></div>
         </div>
         <div class="mb-3">
             <label for="type_id" class="form-label">Loại</label>
-            <select name="type_id" id="type_id" class="form-select">
+            <select name="type_id" id="type_id" class="form-select <?= (isset($errors['type_id'])) ? 'is-invalid' : '' ?>">
                 <option value="" selected>Chọn loại</option>
                 <?php foreach ($types as $key => $type) : ?>
                     <option value="<?= $type->id ?>"><?= $type->name ?></option>
@@ -19,10 +19,10 @@
         </div>
         <div class="mb-3">
             <label for="content" class="form-label">Nội dung</label>
-            <textarea rows="10" cols="50" class="form-control" name="content" id="content"></textarea>
+            <textarea rows="10" cols="50" class="form-control <?= (isset($errors['content'])) ? 'is-invalid' : '' ?>" name="content" id="content"></textarea>
             <div id="emailHelp" class="form-text" style="color:red"><?= (isset($errors['content'])) ? $errors['content'] : ''; ?></div>
         </div>
         <button type="submit" class="btn btn-success">Lưu</button>
-        <button type="submit" class="btn btn-danger">Hủy</button>
+        <a href="index.php?controller=Home&action=index" class="btn btn-danger">Hủy</a>
     </form>
 </div>
--- app/view/pages/edit.php
+++ app/view/pages/edit.php
@@ -5,12 +5,12 @@
     <form method="post">
         <div class="mb-3">
             <label for="title" class="form-label">Tiêu đề</label>
-            <input type="text" class="form-control" id="title" name="title" value="<?= $note->title ?>">
+            <input type="text" class="form-control <?= (isset($errors['title'])) ? 'is-invalid' : '' ?>" id="title" name="title" value="<?= $note->title ?>">
             <div id="emailHelp" class="form-text" style="color:red"><?= (isset($errors['title'])) ? $errors['title'] : ''; ?></div>
         </div>
         <div class="mb-3">
             <label for="type_id" class="form-label">Loại</label>
-            <select name="type_id" id="type_id" class="form-select">
+            <select name="type_id" id="type_id" class="form-select <?= (isset($errors['type_id'])) ? 'is-invalid' : '' ?>">
                 <option value="" selected>Chọn loại</option>
                 <?php foreach ($types as $key => $type) : ?>
                     <option <?php if ($type->id == $note->type_id) {
@@ -22,7 +22,7 @@
         </div>
         <div class="mb-3">
             <label for="content" class="form-label">Nội dung</label>
-            <textarea rows="10" cols="50" class="form-control" name="content" id="content"><?= $note->content ?></textarea>
+            <textarea rows="10" cols="50" class="form-control <?= (isset($errors['content'])) ? 'is-invalid' : '' ?>" name="content" id="content"><?= $note->content ?></textarea>
             <div id="emailHelp" class="form-text" style="color:red"><?= (isset($errors['content'])) ? $errors['content'] : ''; ?></div>
         </div>
         <button type="submit" class="btn btn-success">Lưu</button>
